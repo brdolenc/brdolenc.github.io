eddddd256b8ed70507bbaa391e4392862e10a79b
BrunoDolenc <bruno@ionz.com.br>
2018-12-10 18:29:58 -0200

    freela

--- porsche/files/js/main.js
+++ porsche/files/js/main.js
@@ -3,7 +3,8 @@ $(document).ready(function(){
 	$('.slides').slick({
   		dots:true,
   		infinite: true,
-  		speed: 1000
+  		speed: 1000,
+  		adaptiveHeight: true
 	}).on('beforeChange', function(event, slick, currentSlide, nextSlide){
 	  	
 	  	$("#item-info-"+currentSlide).fadeOut('slow', function(){
